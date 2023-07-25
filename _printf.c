e70e7fc3d34c2f832617ec80e7aaa12829552d1c
mohamed <zakei7671@gmail.com>
2023-07-25 06:07:26 -0700

    j

--- _printf.c
+++ _printf.c
@@ -1,22 +1,138 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include "main.h"
+
+/* ... Other function implementations ... */
+
 /**
- * _putchar - Writes a character to the standard output (file descriptor 1)
- * @c: The character to write
- *Return: On success, 1. On error, -1
- *	is returned, and errno is set appropriately.
+ * print_unsigned - Prints an unsigned integer
+ * @args: The va_list containing the integer to be printed
+ *
+ * Return: The number of characters printed (excluding the null byte used to end output to strings)
  */
-int _putchar(char c)
+int print_unsigned(va_list args)
 {
-	return (write(1, &c, 1));
+	unsigned int n = va_arg(args, unsigned int);
+	int count = 0;
+
+	/* Print individual digits (recursively) */
+	if (n / 10 != 0)
+		count += print_unsigned_helper(n / 10);
+
+	_putchar('0' + n % 10);
+	count++;
+
+	return count;
 }
+
+/**
+ * print_unsigned_helper - Helper function to print an unsigned integer
+ * @n: The integer to be printed
+ *
+ * Return: The number of characters printed (excluding the null byte used to end output to strings)
+ */
+int print_unsigned_helper(unsigned int n)
+{
+	int count = 0;
+
+	if (n / 10 != 0)
+		count += print_unsigned_helper(n / 10);
+
+	_putchar('0' + n % 10);
+	count++;
+
+	return count;
+}
+
+/**
+ * print_octal - Prints an unsigned integer in octal format
+ * @args: The va_list containing the integer to be printed
+ *
+ * Return: The number of characters printed (excluding the null byte used to end output to strings)
+ */
+int print_octal(va_list args)
+{
+	unsigned int n = va_arg(args, unsigned int);
+	int count = 0;
+	char *octal_digits = "01234567";
+	char octal_buffer[12]; /* Enough to hold the octal representation of UINT_MAX + 1 */
+
+	int i = sizeof(octal_buffer) - 1;
+	octal_buffer[i] = '\0';
+
+	do {
+		i--;
+		octal_buffer[i] = octal_digits[n % 8];
+		n /= 8;
+	} while (n > 0);
+
+	_putchar(octal_buffer + i);
+	count += sizeof(octal_buffer) - 1 - i;
+
+	return count;
+}
+
+/**
+ * print_hex - Prints an unsigned integer in hexadecimal format (lowercase)
+ * @args: The va_list containing the integer to be printed
+ *
+ * Return: The number of characters printed (excluding the null byte used to end output to strings)
+ */
+int print_hex(va_list args)
+{
+	unsigned int n = va_arg(args, unsigned int);
+	int count = 0;
+	char *hex_digits = "0123456789abcdef";
+	char hex_buffer[9]; /* Enough to hold the hexadecimal representation of UINT_MAX + 1 */
+
+	int i = sizeof(hex_buffer) - 1;
+	hex_buffer[i] = '\0';
+
+	do {
+		i--;
+		hex_buffer[i] = hex_digits[n % 16];
+		n /= 16;
+	} while (n > 0);
+
+	_putchar(hex_buffer + i);
+	count += sizeof(hex_buffer) - 1 - i;
+
+	return count;
+}
+
+/**
+ * print_HEX - Prints an unsigned integer in hexadecimal format (uppercase)
+ * @args: The va_list containing the integer to be printed
+ *
+ * Return: The number of characters printed (excluding the null byte used to end output to strings)
+ */
+int print_HEX(va_list args)
+{
+	unsigned int n = va_arg(args, unsigned int);
+	int count = 0;
+	char *hex_digits = "0123456789ABCDEF";
+	char hex_buffer[9]; /* Enough to hold the hexadecimal representation of UINT_MAX + 1 */
+
+	int i = sizeof(hex_buffer) - 1;
+	hex_buffer[i] = '\0';
+
+	do {
+		i--;
+		hex_buffer[i] = hex_digits[n % 16];
+		n /= 16;
+	} while (n > 0);
+
+	_putchar(hex_buffer + i);
+	count += sizeof(hex_buffer) - 1 - i;
+
+	return count;
+}
+
 /**
  * _printf - Produces output according to a format.
  * @format: A character string containing zero or more directives.
  *
- * Return: The number of characters printed
- *	(excluding the null byte used to end output to strings)
+ * Return: The number of characters printed (excluding the null byte used to end output to strings)
  */
 int _printf(const char *format, ...)
 {
@@ -24,49 +140,64 @@ int _printf(const char *format, ...)
 	int count = 0;
 
 	va_start(args, format);
+
 	while (*format)
 	{
-	if (*format != '%')
-	{
-	_putchar(*format);
-	count++;
-	}
-	else
-	{
-	format++;
-	switch (*format)
-	{
-	case 'c':
-	_putchar(va_arg(args, int));
-	count++;
-	break;
-	case 's':
-	{
-	char *str = va_arg(args, char*);
+		if (*format != '%')
+		{
+			_putchar(*format);
+			count++;
+		}
+		else
+		{
+			format++; /* Move past '%' */
 
-	if (str == NULL)
-	str = "(null)";
-	while (*str)
-	{
-	_putchar(*str);
-	str++;
-	count++;
+			switch (*format)
+			{
+				case 'c':
+					count += print_char(args);
+					break;
+
+				case 's':
+					count += print_string(args);
+					break;
+
+				case '%':
+					count += print_percent(args);
+					break;
+
+				case 'd': /* Fallthrough */
+				case 'i':
+					count += print_integer(args);
+					break;
+
+				case 'u':
+					count += print_unsigned(args);
+					break;
+
+				case 'o':
+					count += print_octal(args);
+					break;
+
+				case 'x':
+					count += print_hex(args);
+					break;
+
+				case 'X':
+					count += print_HEX(args);
+					break;
+
+				default:
+					_putchar('%');
+					_putchar(*format);
+					count += 2;
+			}
+		}
+
+		format++;
 	}
-	break;
-	}
-	case '%':
-	_putchar('%');
-	count++;
-	break;
-	default:
-	_putchar('%');
-	_putchar(*format);
-	count += 2;
-	}
-	}
-	format++;
-	}
+
 	va_end(args);
-	return (count);
+	return count;
 }
 
