992cb3061a06c43d468cd276fe52a61f139d61ce
mohamed <zakei7671@gmail.com>
2023-07-25 05:44:45 -0700

    j

--- _printf.c
+++ _printf.c
@@ -1,58 +1,79 @@
-#include <stdio.h>
+#include <unistd.h>
 #include <stdarg.h>
+#include "main.h"
 
-int _printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
-
-    int count = 0;
-
-    while (*format != '\0') {
-        if (*format == '%') {
-            format++; // Move past '%'
-
-            // Check for conversion specifiers
-            switch (*format) {
-                case 'c': {
-                    int c = va_arg(args, int);
-                    putchar(c);
-                    count++;
-                    break;
-                }
-                case 's': {
-                    char *s = va_arg(args, char*);
-                    while (*s != '\0') {
-                        putchar(*s);
-                        s++;
-                        count++;
-                    }
-                    break;
-                }
-                case '%':
-                    putchar('%');
-                    count++;
-                    break;
-                default:
-                    putchar('%'); // Print the '%' character
-                    putchar(*format); // Print the unrecognized character
-                    count += 2;
-            }
-        } else {
-            putchar(*format);
-            count++;
-        }
-
-        format++;
-    }
-
-    va_end(args);
-    return count;
+/**
+ * _putchar - Writes a character to the standard output (file descriptor 1)
+ * @c: The character to write
+ *
+ * Return: On success, 1. On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return write(1, &c, 1);
 }
 
-// Example usage:
-int main() {
-    int num_chars = _printf("I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life %%\n");
-    printf("\nNumber of characters printed: %d\n", num_chars);
-    return 0;
+/**
+ * _printf - Produces output according to a format.
+ * @format: A character string containing zero or more directives.
+ *
+ * Return: The number of characters printed (excluding the null byte used to end output to strings)
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			_putchar(*format);
+			count++;
+		}
+		else
+		{
+			format++; /* Move past '%' */
+
+			switch (*format)
+			{
+				case 'c':
+					_putchar(va_arg(args, int));
+					count++;
+					break;
+
+				case 's':
+				{
+					char *str = va_arg(args, char*);
+					if (str == NULL)
+						str = "(null)";
+					while (*str)
+					{
+						_putchar(*str);
+						str++;
+						count++;
+					}
+					break;
+				}
+
+				case '%':
+					_putchar('%');
+					count++;
+					break;
+
+				default:
+					_putchar('%');
+					_putchar(*format);
+					count += 2;
+			}
+		}
+
+		format++;
+	}
+
+	va_end(args);
+	return count;
 }
 
