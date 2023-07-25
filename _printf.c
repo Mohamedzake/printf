416c43cdf934dab8d12000cb7dfbc82fec0dab49
mohamed <zakei7671@gmail.com>
2023-07-25 06:20:11 -0700

    h

--- _printf.c
+++ _printf.c
@@ -1,138 +1,100 @@
-#include <unistd.h>
-#include <stdarg.h>
 #include "main.h"
-
-/* ... Other function implementations ... */
+#include <stdarg.h>
+#include <unistd.h>
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to write
+ *
+ * Return: On success, the number of characters written.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
- * print_unsigned - Prints an unsigned integer
- * @args: The va_list containing the integer to be printed
+ * print_char - Prints a character
+ * @args: The va_list that contains the character to print
  *
- * Return: The number of characters printed (excluding the null byte used to end output to strings)
+ * Return: The number of characters printed.
  */
-int print_unsigned(va_list args)
+int print_char(va_list args)
 {
-	unsigned int n = va_arg(args, unsigned int);
+	char c = va_arg(args, int);
+	_putchar(c);
+	return 1;
+}
+
+/**
+ * print_string - Prints a string
+ * @args: The va_list that contains the string to print
+ *
+ * Return: The number of characters printed.
+ */
+int print_string(va_list args)
+{
+	char *str = va_arg(args, char*);
 	int count = 0;
 
-	/* Print individual digits (recursively) */
-	if (n / 10 != 0)
-		count += print_unsigned_helper(n / 10);
+	if (str == NULL)
+		str = "(null)";
 
-	_putchar('0' + n % 10);
-	count++;
+	while (*str)
+	{
+		_putchar(*str);
+		count++;
+		str++;
+	}
 
 	return count;
 }
 
 /**
- * print_unsigned_helper - Helper function to print an unsigned integer
- * @n: The integer to be printed
+ * print_percent - Prints a percent character
+ * @args: The va_list (unused)
  *
- * Return: The number of characters printed (excluding the null byte used to end output to strings)
+ * Return: Always 1 (as only one character is printed)
  */
-int print_unsigned_helper(unsigned int n)
+int print_percent(__attribute__((unused)) va_list args)
 {
-	int count = 0;
-
-	if (n / 10 != 0)
-		count += print_unsigned_helper(n / 10);
-
-	_putchar('0' + n % 10);
-	count++;
-
-	return count;
+	_putchar('%');
+	return 1;
 }
 
 /**
- * print_octal - Prints an unsigned integer in octal format
- * @args: The va_list containing the integer to be printed
+ * print_integer - Prints an integer
+ * @args: The va_list that contains the integer to print
  *
- * Return: The number of characters printed (excluding the null byte used to end output to strings)
+ * Return: The number of characters printed.
  */
-int print_octal(va_list args)
+int print_integer(va_list args)
 {
-	unsigned int n = va_arg(args, unsigned int);
-	int count = 0;
-	char *octal_digits = "01234567";
-	char octal_buffer[12]; /* Enough to hold the octal representation of UINT_MAX + 1 */
+    int n = va_arg(args, int);
+    int count = 0;
 
-	int i = sizeof(octal_buffer) - 1;
-	octal_buffer[i] = '\0';
+    if (n < 0)
+    {
+        count += _putchar('-');
+        n = -n;
+    }
 
-	do {
-		i--;
-		octal_buffer[i] = octal_digits[n % 8];
-		n /= 8;
-	} while (n > 0);
+    if (n / 10 != 0)
+    {
+        count += print_integer(args);
+    }
 
-	_putchar(octal_buffer + i);
-	count += sizeof(octal_buffer) - 1 - i;
+    count += _putchar('0' + n % 10);
 
-	return count;
+    return count;
 }
 
 /**
- * print_hex - Prints an unsigned integer in hexadecimal format (lowercase)
- * @args: The va_list containing the integer to be printed
+ * _printf - Produces output according to a format
+ * @format: The format string
  *
- * Return: The number of characters printed (excluding the null byte used to end output to strings)
- */
-int print_hex(va_list args)
-{
-	unsigned int n = va_arg(args, unsigned int);
-	int count = 0;
-	char *hex_digits = "0123456789abcdef";
-	char hex_buffer[9]; /* Enough to hold the hexadecimal representation of UINT_MAX + 1 */
-
-	int i = sizeof(hex_buffer) - 1;
-	hex_buffer[i] = '\0';
-
-	do {
-		i--;
-		hex_buffer[i] = hex_digits[n % 16];
-		n /= 16;
-	} while (n > 0);
-
-	_putchar(hex_buffer + i);
-	count += sizeof(hex_buffer) - 1 - i;
-
-	return count;
-}
-
-/**
- * print_HEX - Prints an unsigned integer in hexadecimal format (uppercase)
- * @args: The va_list containing the integer to be printed
- *
- * Return: The number of characters printed (excluding the null byte used to end output to strings)
- */
-int print_HEX(va_list args)
-{
-	unsigned int n = va_arg(args, unsigned int);
-	int count = 0;
-	char *hex_digits = "0123456789ABCDEF";
-	char hex_buffer[9]; /* Enough to hold the hexadecimal representation of UINT_MAX + 1 */
-
-	int i = sizeof(hex_buffer) - 1;
-	hex_buffer[i] = '\0';
-
-	do {
-		i--;
-		hex_buffer[i] = hex_digits[n % 16];
-		n /= 16;
-	} while (n > 0);
-
-	_putchar(hex_buffer + i);
-	count += sizeof(hex_buffer) - 1 - i;
-
-	return count;
-}
-
-/**
- * _printf - Produces output according to a format.
- * @format: A character string containing zero or more directives.
- *
- * Return: The number of characters printed (excluding the null byte used to end output to strings)
+ * Return: The number of characters printed (excluding the null byte used to
+ * end output to strings).
  */
 int _printf(const char *format, ...)
 {
@@ -141,7 +103,7 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	while (*format)
+	while (format && *format)
 	{
 		if (*format != '%')
 		{
@@ -150,47 +112,31 @@ int _printf(const char *format, ...)
 		}
 		else
 		{
-			format++; /* Move past '%' */
+			format++;
+
+			if (*format == '\0')
+				break;
 
 			switch (*format)
 			{
-				case 'c':
-					count += print_char(args);
-					break;
-
-				case 's':
-					count += print_string(args);
-					break;
-
-				case '%':
-					count += print_percent(args);
-					break;
-
-				case 'd': /* Fallthrough */
-				case 'i':
-					count += print_integer(args);
-					break;
-
-				case 'u':
-					count += print_unsigned(args);
-					break;
-
-				case 'o':
-					count += print_octal(args);
-					break;
-
-				case 'x':
-					count += print_hex(args);
-					break;
-
-				case 'X':
-					count += print_HEX(args);
-					break;
-
-				default:
-					_putchar('%');
-					_putchar(*format);
-					count += 2;
+			case 'c':
+				count += print_char(args);
+				break;
+			case 's':
+				count += print_string(args);
+				break;
+			case '%':
+				count += print_percent(args);
+				break;
+			case 'i':
+			case 'd':
+				count += print_integer(args);
+				break;
+			default:
+				_putchar('%');
+				_putchar(*format);
+				count += 2;
+				break;
 			}
 		}
 
